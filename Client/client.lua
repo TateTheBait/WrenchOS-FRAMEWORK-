61d6ce4adc24c37c3600dcdc183746355e5f25fa
The Wrench <123601564+TateTheBait@users.noreply.github.com>
2024-08-30 09:36:42 -0500

    Update client.lua

--- Client/client.lua
+++ Client/client.lua
@@ -4,3 +4,13 @@ TriggerServerEvent("WrenchOS:PlayerJoined", GetPlayerServerId(PlayerId()))
 RegisterCommand("ostest1", function(src, args)
     print(exports.WrenchOS:addMoney(GetPlayerServerId(PlayerId()), "cash", 10))
 end, false)
+
+
+
+
+-- Command suggestions
+
+TriggerEvent('chat:addSuggestion', '/changejob', 'changes WrenchOS Job', {
+    { name="playerid", help="Players server id" },
+    { name="jobname", help="EX: STAFF, LEO etc." }
+})
