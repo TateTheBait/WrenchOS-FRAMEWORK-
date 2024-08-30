4385cb80d252a6487c7f49606b268aed8b53a3f6
The Wrench <123601564+TateTheBait@users.noreply.github.com>
2024-08-30 09:37:52 -0500

    Update config.lua

--- config.lua
+++ config.lua
@@ -3,7 +3,7 @@ Config = {}
 
 Config.paychecks = true -- use paychecks (If true then causes server ms to go from 0.0 to like 0.001)
 
-Config.paycheckJobs = { ["STAFF"] = 5000, ["CIV"] = 2500, ["LEO"] = 5000 } -- How much each job pays
+Config.paycheckJobs = { ["STAFF"] = 5000, ["CIV"] = 2500, ["LEO"] = 5000, ["SAFR"] = 5000, ["HIGHSTAFF"] = 10000 } -- How much each job pays
 
 Config.startingCash = 2000
 Config.startingBank = 2000
